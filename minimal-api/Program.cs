1c248f75c2754d18060f5b4c0d769679c6c204b4
Oskar De Clerck <oskar.de.clerck@student.howest.be>
2025-03-28 10:30:31 +0100

    Adding new endpoint

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -3,5 +3,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello from Azure!");
 app.MapGet("/oskar", () => "Hello from Oskar!");
+app.MapGet("/roennie", () => new 
+{
+    name = "Roennie",
+    address = "Mouterijstraat 26",
+    car = "Ford Fiesta",
+    maite = "Yes"
+});
 
 app.Run();
